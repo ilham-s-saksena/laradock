89d4dd84a36940ef31ec5fbfc0aca33b4dd9b680
Ilham Sidik <ilhamsidikc@gmail.com>
2024-03-04 10:38:37 +0700

    connect to mysql database

--- install.sh
+++ install.sh
@@ -1,3 +1,16 @@
+echo "______  _  _    _              "
+echo "| ___ \(_)| |  (_)             "
+echo "| |_/ / _ | |   _  _   _  _ __ "
+echo "|  __/ | || |  | || | | || '__|"
+echo "| |    | || |  | || |_| || |   "
+echo "\_|    |_||_|  | | \__,_||_|   "
+echo "              _/ |             "
+echo "             |__/              "
+echo "--------------------------------"
+echo "--Start Instaling Dependencies--"
+echo "--------------------------------"
+
+
 docker run --rm \
   -v "$(dirname $(readlink -f $0))/src:/var/www/html" \
   -w /var/www/html \
@@ -6,10 +19,6 @@ docker run --rm \
     composer install
 
     php artisan env:decrypt --key=6UVsEgGVK36XN82KKeyLFMhvosbZN1aF
-
-    php -m | grep dom
-
-    php -m | grep mysql
 
     chmod -R 777 storage
 
